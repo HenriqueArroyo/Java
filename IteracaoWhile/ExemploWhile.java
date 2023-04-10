aa29b6654f39d9fd64d20a265fb1b2a500db1cd5
HenriqueArroyo <ana123ca.henrique@gmail.com>
2023-04-10 14:00:04 -0300

    10/04

---
+++ IteracaoWhile/ExemploWhile.java
@@ -0,0 +1,14 @@
+package IteracaoWhile;
+
+import java.util.Scanner;
+
+public class ExemploWhile {
+    Scanner sc = new Scanner(System.in);
+    public void exemplo1 () {
+    int i = 0;
+    while (i<10) {
+        System.out.println("o nº da iteração é "+i);
+        i++;
+    }
+    }
+}
