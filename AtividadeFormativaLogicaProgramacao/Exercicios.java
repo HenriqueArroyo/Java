5fc763f372d2c4c0b671a7653447c9b823e8183e
luczinn <lucfpscs@gmail.com>
2023-05-22 14:24:09 -0300

    22/05/23

---
+++ AtividadeFormativaLogicaProgramacao/Exercicios.java
@@ -0,0 +1,128 @@
+package AtividadeFormativaLogicaProgramacao;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Exercicios {
+    Scanner sc = new Scanner(System.in);
+    Random rd = new Random();
+    public void Exercicio1() {
+        System.out.print("Determine o Nº de linhas: ");
+        int linhas = sc.nextInt();
+
+        System.out.print("Determine o Nº de colunas: ");
+        int colunas = sc.nextInt();
+
+        int[][] matriz = new int[linhas][colunas];
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                matriz[i][j] = rd.nextInt(9) + 1;
+            }
+        }
+
+        System.out.println("Matriz Preenchida:");
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Matriz após substituição dos valores:");
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                if (i > j) {
+                    matriz[i][j] = 1;
+                } else if (i == j) {
+                    matriz[i][j] = 0;
+                } else {
+                    matriz[i][j] = 2;
+                }
+            }
+        }
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public void Exercicio2() {
+        int numeroSorteado = (int) (Math.random() * 1001);
+        int palpite;
+        int tentativas = 0;
+      
+        System.out.println("Tente adivinhar um número de 0 a 1000.");
+
+        while (true) {
+            System.out.print("Digite o seu palpite: ");
+            palpite = sc.nextInt();
+            tentativas++;
+
+            if (palpite == numeroSorteado) {
+              
+                System.out.println("Parabéns! Você acertou o número " + numeroSorteado + ".");
+                System.out.println("Você acertou em " + tentativas + " tentativa(s).");
+            } else if (palpite < numeroSorteado) {
+                System.out.println("O número sorteado é maior do que o seu palpite.");
+            } else {
+                System.out.println("O número sorteado é menor do que o seu palpite.");
+            }
+        }
+    }
+    public void Exercicio3() {
+        int tamanhoVetor = rd.nextInt(901) + 100; 
+        int[] vetor = new int[tamanhoVetor];
+
+        System.out.println("Tamanho do vetor: " + tamanhoVetor);
+
+        System.out.println("Vetor gerado:");
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = rd.nextInt(100) + 1; 
+            System.out.print(vetor[i] + " ");
+        }
+
+        System.out.println("Números pares do vetor:");
+
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] % 2 == 0) {
+                System.out.print(vetor[i] + " ");
+            }
+        }
+
+        System.out.println("Números ímpares do vetor:");
+
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] % 2 != 0) {
+                System.out.print(vetor[i] + " ");
+            }
+        }
+
+        int quantidadeParesPosicoesImpares = 0;
+        int quantidadeImparesPosicoesPares = 0;
+
+        for (int i = 1; i < vetor.length; i += 2) {
+            if (vetor[i] % 2 == 0) {
+                quantidadeParesPosicoesImpares++;
+            }
+        }
+
+        for (int i = 0; i < vetor.length; i += 2) {
+            if (vetor[i] % 2 != 0) {
+                quantidadeImparesPosicoesPares++;
+            }
+        }
+
+        System.out.println("Quantidade de números pares nas posições ímpares: " + quantidadeParesPosicoesImpares);
+        System.out.println("Quantidade de números ímpares nas posições pares: " + quantidadeImparesPosicoesPares);
+    }
+}
+    
+        
+    
+
